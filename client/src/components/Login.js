aeaf2e79e50c763319a34651a38f8f388c5518fc
SanketBhosle <110601682+Sanketbhosle1103@users.noreply.github.com>
2024-10-02 19:09:30 +0530

    added vercel endpoint login.js

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -32,7 +32,7 @@ function Login() {
     }
 
     try {
-      const response = await fetch("http://localhost:5001/api/login", {
+      const response = await fetch("https://mealify-api.vercel.app/api/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
