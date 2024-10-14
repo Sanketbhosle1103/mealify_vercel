42db4b9965d71ee6169127092ade6a96e944ca8f
SanketBhosle <110601682+Sanketbhosle1103@users.noreply.github.com>
2024-10-15 01:42:22 +0530

    Update firebase.js

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -3,13 +3,20 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  apiKey: "AIzaSyB7H-6YzHTQw5joEww7XnQzxBP2L50GcyE",
+  authDomain: "travel-itinerary-98796.firebaseapp.com",
+  projectId: "travel-itinerary-98796",
+  storageBucket: "travel-itinerary-98796.appspot.com",
+  messagingSenderId: "840025547218",
+  appId: "1:840025547218:web:1bec7bf88d22b63920680b",
 };
+
+// REACT_APP_FIREBASE_API_KEY=AIzaSyB7H-6YzHTQw5joEww7XnQzxBP2L50GcyE
+// REACT_APP_FIREBASE_AUTH_DOMAIN=travel-itinerary-98796.firebaseapp.com
+// REACT_APP_FIREBASE_PROJECT_ID=travel-itinerary-98796
+// REACT_APP_FIREBASE_STORAGE_BUCKET=travel-itinerary-98796.appspot.com
+// REACT_APP_FIREBASE_MESSAGING_SENDER_ID=840025547218
+// REACT_APP_FIREBASE_APP_ID=1:840025547218:web:1bec7bf88d22b63920680b
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
