e4c9e75c7178c193f85814453a061d3940076b76
SanketBhosle <110601682+Sanketbhosle1103@users.noreply.github.com>
2024-10-15 01:32:57 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -18,24 +18,12 @@ const corsOptions = {
   methods: "GET, POST, OPTIONS, PUT, DELETE",
   allowedHeaders: ["Content-Type", "X-Auth-Token", "Origin", "Authorization"],
 };
- 
+
 app.use(cors(corsOptions));
-
-// const corsOptions = {
-//   origin: process.env.NODE_ENV === "production" 
-//     ? "https://mealify-vercel.vercel.app" 
-//     : "http://localhost:3000",
-//   credentials: true,
-//   methods: "GET, POST, OPTIONS, PUT, DELETE",
-//   allowedHeaders: ["Content-Type", "X-Auth-Token", "Origin", "Authorization"],
-// };
-
-// app.use(cors(corsOptions));
-
 
 mongoose
   .connect(
-    `mongodb+srv://sanketbhosle:sanketbhosle@cluster0.anj8aqs.mongodb.net/mealify?retryWrites=true&w=majority&appName=Cluster0`
+   `mongodb+srv://sanketbhosle:sanketbhosle@cluster0.anj8aqs.mongodb.net/mealify?retryWrites=true&w=majority&appName=Cluster0`
   )
   .then(() => {
     console.log("CONNECTED TO MONGODB");
@@ -47,8 +35,12 @@ mongoose
 
 app.use("/api/", authRouter);
 app.use("/api/", mealRouter);
- 
+
 const PORT = process.env.PORT || 5001;
+
+app.get("/", (req, res) => {
+  res.status(200).send("Welcome to Mealify");
+});
 
 app.listen(PORT, () => {
   console.log("server is starting on "+PORT);
@@ -63,7 +55,7 @@ app.listen(PORT, () => {
 // const authRouter = require("./routes/authroutes.js");
 // const mealRouter = require("./routes/mealroutes.js");
 // require("dotenv").config();
- 
+
 // app.use(cookieParser());
 // app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({ extended: false }));
@@ -77,10 +69,21 @@ app.listen(PORT, () => {
  
 // app.use(cors(corsOptions));
 
+// // const corsOptions = {
+// //   origin: process.env.NODE_ENV === "production" 
+// //     ? "https://mealify-vercel.vercel.app" 
+// //     : "http://localhost:3000",
+// //   credentials: true,
+// //   methods: "GET, POST, OPTIONS, PUT, DELETE",
+// //   allowedHeaders: ["Content-Type", "X-Auth-Token", "Origin", "Authorization"],
+// // };
+
+// // app.use(cors(corsOptions));
+
+
 // mongoose
 //   .connect(
-//    // `mongodb+srv://sanketbhosle:sanketbhosle@cluster0.anj8aqs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
-//     `mongodb+srv://knightclub538:100222gaurav@cluster0.czvsxsu.mongodb.net/cpnew?retryWrites=true&w=majority&appName=Cluster0`
+//     `mongodb+srv://sanketbhosle:sanketbhosle@cluster0.anj8aqs.mongodb.net/mealify?retryWrites=true&w=majority&appName=Cluster0`
 //   )
 //   .then(() => {
 //     console.log("CONNECTED TO MONGODB");
@@ -95,10 +98,55 @@ app.listen(PORT, () => {
  
 // const PORT = process.env.PORT || 5001;
 
-// app.get("/", (req, res) => {
-//   res.status(200).send("Welcome to Mealify");
-// });
-
 // app.listen(PORT, () => {
 //   console.log("server is starting on "+PORT);
 // });
+
+// // const express = require("express");
+// // const app = express();
+// // const bodyParser = require("body-parser");
+// // const cors = require("cors");
+// // const mongoose = require("mongoose");
+// // const cookieParser = require("cookie-parser");
+// // const authRouter = require("./routes/authroutes.js");
+// // const mealRouter = require("./routes/mealroutes.js");
+// // require("dotenv").config();
+ 
+// // app.use(cookieParser());
+// // app.use(bodyParser.json());
+// // app.use(bodyParser.urlencoded({ extended: false }));
+
+// // const corsOptions = {
+// //   origin: ["https://mealify-vercel.vercel.app", "http://localhost:3000"],
+// //   credentials: true,
+// //   methods: "GET, POST, OPTIONS, PUT, DELETE",
+// //   allowedHeaders: ["Content-Type", "X-Auth-Token", "Origin", "Authorization"],
+// // };
+ 
+// // app.use(cors(corsOptions));
+
+// // mongoose
+// //   .connect(
+// //    // `mongodb+srv://sanketbhosle:sanketbhosle@cluster0.anj8aqs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+// //     `mongodb+srv://knightclub538:100222gaurav@cluster0.czvsxsu.mongodb.net/cpnew?retryWrites=true&w=majority&appName=Cluster0`
+// //   )
+// //   .then(() => {
+// //     console.log("CONNECTED TO MONGODB");
+// //   })
+// //   .catch((err) => {
+// //     console.error("FAILED TO CONNECT TO MONGODB");
+// //     console.error(err);
+// //   });
+
+// // app.use("/api/", authRouter);
+// // app.use("/api/", mealRouter);
+ 
+// // const PORT = process.env.PORT || 5001;
+
+// // app.get("/", (req, res) => {
+// //   res.status(200).send("Welcome to Mealify");
+// // });
+
+// // app.listen(PORT, () => {
+// //   console.log("server is starting on "+PORT);
+// // });
